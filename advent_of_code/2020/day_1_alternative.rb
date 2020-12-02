d81555938e2dada29c8cabd86bf1024fefdf0df5
Ale Paredes <alexandra@codeclimate.com>
2020-12-02 00:20:18 -0500

    some clean up

--- advent_of_code/2020/day_1_alternative.rb
+++ advent_of_code/2020/day_1_alternative.rb
@@ -1,16 +1,16 @@
 expenses = File.read("input.txt").split.map(&:to_i).sort!
 
-def get_sum(values, start, target, pivot: nil)
-  sum = nil
+def get_pair(values, start, target)
+  pair = nil
   final = -1
 
-  while sum.nil? do
+  while pair.nil? do
     break if values[start].nil? || values[final].nil?
 
-    tentative_result = (pivot || 0) + values[start] + values[final]
+    tentative_result = values[start] + values[final]
 
     if tentative_result == target
-      sum = (pivot || 1) * values[start] * values[final]
+      pair =  [values[start], values[final]]
     elsif tentative_result > target
       final = final - 1
     elsif tentative_result < target
@@ -18,14 +18,15 @@ def get_sum(values, start, target, pivot: nil)
     end
   end
 
-  return sum
+  return pair
 end
 
-puts "Part 1 answer: #{get_sum(expenses, 0, 2020)}"
+puts "Part 1 answer: #{get_pair(expenses, 0, 2020).inject(:*)}"
 
 answer_part_2 = expenses.each_with_index do |current_outer, index|
-  result = get_sum(expenses, index, 2020, pivot: current_outer)
-  break result unless result.nil?
+  pair = get_pair(expenses, index + 1, 2020 - current_outer)
+
+  break pair.inject(:*) * current_outer unless pair.nil?
 end
 
 puts "Part 2 answer: #{answer_part_2}"
